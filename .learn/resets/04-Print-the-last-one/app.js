1d4b805f27d074bd70f165ee1befd6149d2a63f0
willdodsonn <sodlax38@gmail.com>
2021-12-25 23:35:23 +0000

    seventh commit

--- .learn/resets/04-Print-the-last-one/app.js
+++ .learn/resets/04-Print-the-last-one/app.js
@@ -8,8 +8,3 @@ function generateRandomArray()
 var myStupidArray = generateRandomArray();
 
 //Your code here
-//var theLastOne = myStupidArray.length;
-//console.log(myStupidArray.length[3])
-let lastPosition = myStupidArray.length - 1;
-let theLastOne = myStupidArray[lastPosition];
-console.log(myStupidArray[theLastOne])
--- exercises/03.2-Retrieve-Items/app.js
+++ exercises/03.2-Retrieve-Items/app.js
@@ -1,3 +1,9 @@
 var arr = [4,5,734,43,45,100,4,56,23,67,23,58,45,3,100,4,56,23];
 
 //your code here
+var myValue = arr[0];
+console.log(arr[0])
+
+
+var myValue = arr[3];
+console.log(arr[3])

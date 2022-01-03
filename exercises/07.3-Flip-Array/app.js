d73380ff1140b8c9f85e9f3d894433e6e06456bf
willdodsonn <sodlax38@gmail.com>
2022-01-03 21:21:38 +0000

    seventh commit

--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -1,3 +1,10 @@
 var arr = [45,67,87,23,5,32,60];
 
 //Your code here.
+let oppositeArr = []
+for(let i = arr.length-1; i >= 0; i--){
+    let item = arr[i];
+    oppositeArr.push(item);
+    
+}
+console.log(oppositeArr)

643f06ae8f127d2083e2a85649bb6893b455582d
willdodsonn <sodlax38@gmail.com>
2022-01-31 15:43:17 +0000

    loops commit

--- exercises/11-Do-while/app.js
+++ exercises/11-Do-while/app.js
@@ -2,13 +2,14 @@ let i = 20;
 
 do {
     // Magic goes here;
-    console.log(i);
-    if(i%5===0){
-        console.log("!")
+    var additionalText = "";
+    if(i===0){
+        console.log("LIFTOFF");
+        break;
     }
-    else if(i==0){
-        console.log("LIFTOFF")
+    else if(i%5===0){
+       additionalText="!";
     }
-    
+    console.log(i+additionalText);
     i--;
-} while (i > 0);
+} while (i >= 0);

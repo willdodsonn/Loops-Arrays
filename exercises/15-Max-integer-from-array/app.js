42d731ab21c1dce6cd14096bb0b7ca36ce66e663
willdodsonn <sodlax38@gmail.com>
2022-02-01 13:57:22 +0000

    arrays commit

--- exercises/15-Max-integer-from-array/app.js
+++ exercises/15-Max-integer-from-array/app.js
@@ -2,7 +2,7 @@ var myArray = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
 function findMax(array){
     let max = 0;
     let a = array.length;
-    for (counter=0;counter<a;counter++)
+    for (let counter=0;counter<a;counter++)
     {
         if (array[counter] > max)
         {

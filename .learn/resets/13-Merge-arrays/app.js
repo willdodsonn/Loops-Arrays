626f57df2ef60af99ee6fc6f13f740090fda372a
willdodsonn <sodlax38@gmail.com>
2022-02-01 00:41:11 +0000

    loops commit

--- .learn/resets/13-Merge-arrays/app.js
+++ .learn/resets/13-Merge-arrays/app.js
@@ -1,10 +1,9 @@
 var chunk_one = [ 'Lebron', 'Aaliyah', 'Diamond', 'Dominique', 'Aliyah', 'Jazmin', 'Darnell' ];
 var chunk_two = [ 'Lucas' , 'Jake','Scott','Amy', 'Molly','Hannah','Lucas'];
 
-const mergeArrays = (chunk_one, chunk_two) => {
-    let newArray = [].concat(chunk_one, chunk_two);
+const mergeArrays = (firstArray, secondArray) => {
+    let newArray = []
     //your code here
-  
     return newArray
 }
 

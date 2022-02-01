2ed77444294cd70154790cf9f6ebb1e274ea5813
willdodsonn <sodlax38@gmail.com>
2022-02-01 13:44:19 +0000

    arrays commit

--- exercises/12-Delete-element/app.js
+++ exercises/12-Delete-element/app.js
@@ -1,14 +1,11 @@
-var people = ['juan', 'ana', 'michelle', 'daniella', 'stefany', 'lucy', 'barak', 'emilio'];
+var people = ['juan','ana','michelle','daniella','stefany','lucy','barak', 'emilio'];
 
 //your code below
-const deletePerson = () => {
-    let newArray = [];
-    for (let i = 0; i < people.length; i++) {
-        people.length = people.filter(value => value == '');
-        return people.length;
-    }
-    newArray.push(people.length);
+const deletePerson = (personToDelete) => {
+    let resultArray = people.filter((element) => element !== personToDelete);
+    return resultArray;
 }
+
 
 console.log(deletePerson('daniella'));
 console.log(deletePerson('juan'));
--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,6 +1,20 @@
-let list_of_numbers = [4, 80, 85, 59, 37,25, 5, 64, 66,	81,20, 64, 41, 22, 76,76, 55, 96, 2, 68];
-
+let list_of_numbers = [4, 80, 85, 59, 37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76, 55, 96, 2, 68];
+let odd = [];
+let even = [];
 // your code here
+const mergeTwoList = () => {
+    list_of_numbers.forEach((number => {
+        if (number % 2 === 0) {
+            even.push(number);
+        }
+        else {
+            odd.push(number);
+        }
+    }));
+    let newArray = [].concat(odd, even);
+    return newArray;
+}
+//here odd and even are empty
+console.log(mergeTwoList(list_of_numbers));
 
 
-console.log(mergeTwoList(list_of_numbers))

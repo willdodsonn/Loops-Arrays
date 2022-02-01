89b64717b7ff169aaa6ffe42431314b9aee32ce1
willdodsonn <sodlax38@gmail.com>
2022-02-01 04:16:07 +0000

    loops commit

--- exercises/12-Delete-element/app.js
+++ exercises/12-Delete-element/app.js
@@ -1,6 +1,14 @@
-var people = ['juan','ana','michelle','daniella','stefany','lucy','barak', 'emilio'];
+var people = ['juan', 'ana', 'michelle', 'daniella', 'stefany', 'lucy', 'barak', 'emilio'];
 
 //your code below
+const deletePerson = () => {
+    let newArray = [];
+    for (let i = 0; i < people.length; i++) {
+        people.length = people.filter(value => value == '');
+        return people.length;
+    }
+    newArray.push(people.length);
+}
 
 console.log(deletePerson('daniella'));
 console.log(deletePerson('juan'));

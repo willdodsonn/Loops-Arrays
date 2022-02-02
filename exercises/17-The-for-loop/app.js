969eec431f88501a2333041686396b99845be99e
willdodsonn <sodlax38@gmail.com>
2022-02-02 23:06:50 +0000

    loops commit

--- exercises/17-The-for-loop/app.js
+++ exercises/17-The-for-loop/app.js
@@ -1,9 +1,8 @@
 var myArray = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5656,423];
 let total = 0;
 for (var number in myArray){
-	parseInt(number);
-	 total+=number;
 	
+	 total+=number;
+	 let avg = total / myArray.length;
+	 console.log(avg);
 }
-let avg = total / myArray.length;
-console.log(avg);
--- exercises/18-Nested-arrays/app.js
+++ exercises/18-Nested-arrays/app.js
@@ -1,6 +1,11 @@
-var coordinatesArray = [[33.747252,-112.633853],[-33.867886, -63.987],[41.303921, -81.901693],[-33.350534, -71.653268]];
-var longitude = coordinatesArray[0][1];
+var coordinatesArray = [
+    [33.747252,-112.633853],
+    [-33.867886, -63.987],
+    [41.303921, -81.901693],
+    [-33.350534, -71.653268]];
+
 
 for(let i = 0; i < coordinatesArray.length; i++){
-    console.log(coordinatesArray[i]); 
+    var longitude = coordinatesArray[i][1];
+    console.log(longitude); 
 }

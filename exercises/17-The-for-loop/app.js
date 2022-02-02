584c54da912c90614e47b30d710c9df47d0e310d
willdodsonn <sodlax38@gmail.com>
2022-02-02 05:18:11 +0000

    arrays n loops commit

--- exercises/17-The-for-loop/app.js
+++ exercises/17-The-for-loop/app.js
@@ -1,7 +1,9 @@
 var myArray = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5656,423];
-
-for (var index in myArray){
-
-	console.log(myArray[index]);
+let total = 0;
+for (var number in myArray){
+	parseInt(number);
+	 total+=number;
 	
 }
+let avg = total / myArray.length;
+console.log(avg);
--- exercises/19-And-One-and-a-Two-and-a-Three/app.js
+++ exercises/19-And-One-and-a-Two-and-a-Three/app.js
@@ -1,9 +1,9 @@
 let contact = {
-    fullname: "Jane Doe",
-    phone: "321-321-4321",
-    email: "test@test.com"
+    fullname: "John Doe",
+    phone: "123-123-2134",
+    email: "test@nowhere.com"
 }
 for(let key in contact){
-    // Code goes here
+    console.log((`${key}: ${contact[key]}`));
 }
 

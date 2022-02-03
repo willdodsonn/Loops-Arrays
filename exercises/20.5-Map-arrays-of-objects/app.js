90f1adc9296c76e6cf62f9351ecc77aed07a4f59
willdodsonn <sodlax38@gmail.com>
2022-02-03 22:30:37 +0000

    maps commit

--- exercises/20.5-Map-arrays-of-objects/app.js
+++ exercises/20.5-Map-arrays-of-objects/app.js
@@ -5,21 +5,35 @@ let people = [
 	{ name: 'Dylan', birthDate: new Date(1999,12,14) },
 	{ name: 'Steve', birthDate: new Date(2003,4,24) }
 ];
+for(let i = 0; i < people.length; i++){
+	let birthday = people[i];
+	console.log(birthday);
+}
 
-let myName = "Hello my name is "
+let myName = "Hello my name is ";
 let andIn = " and I am ";
-let yearsOld = " years old."
+let yearsOld = " years old.";
 
-const getAgeFromBirthDate = (year, month, day) => {
-	const date = new Date();
-	date.setFullYear(date.getFullYear() - year);
-	date.setMonth(date.getMonth() - month);
-	//date.setDate(date.getDate() - day);
-	return date;
- };
+const getAge = (birthDateString) => {
+	const today = new Date();
+	const dob = new Date(birthDateString);
+  
+	const yearsDifference = today.getFullYear() - dob.getFullYear();
+  
+	if (
+	  today.getMonth() < dob.getMonth() ||
+	  (today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate())
+	) {
+	  return yearsDifference - 1;
+	}
+  
+	return yearsDifference;
+  };
+  
+  console.log(getAge(1986,10,24));
 
 let simplifier = function(person){
-	return myName + person.name + andIn +;
+	return myName + person.name + andIn +  people + yearsOld;
 };
 
 console.log(people.map(simplifier));
--- exercises/20.6-Yes-and-no/app.js
+++ exercises/20.6-Yes-and-no/app.js
@@ -1,3 +1,12 @@
 let theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1];
 
 //your code here
+let newArray = theBools.map((number) =>{
+    if(typeof number === 1){
+        newArray.push("wiki");
+    }
+    else if(typeof number === 0){
+        newArray.push("woko");
+    }
+});
+console.log(newArray);

896ba9909f94e8540b3c3895d546bd8d9123c5ff
willdodsonn <sodlax38@gmail.com>
2022-02-04 19:32:53 +0000

    maps commit

--- exercises/20.5-Map-arrays-of-objects/app.js
+++ exercises/20.5-Map-arrays-of-objects/app.js
@@ -1,40 +1,30 @@
 let people = [
-	{ name: 'Joe', birthDate: new Date(1986,10,24) },
-	{ name: 'Bob', birthDate: new Date(1975,5,24) },
-	{ name: 'Erika', birthDate: new Date(1989,6,12) },
-	{ name: 'Dylan', birthDate: new Date(1999,12,14) },
-	{ name: 'Steve', birthDate: new Date(2003,4,24) }
+	{ name: 'Joe', birthDate: new Date(1986, 10, 24) },
+	{ name: 'Bob', birthDate: new Date(1975, 5, 24) },
+	{ name: 'Erika', birthDate: new Date(1989, 6, 12) },
+	{ name: 'Dylan', birthDate: new Date(1999, 12, 14) },
+	{ name: 'Steve', birthDate: new Date(2003, 4, 24) }
 ];
 
 
-let myName = "Hello my name is ";
+
+let myName = "Hello, my name is ";
 let andIn = " and I am ";
-let yearsOld = " years old.";
+let yearsOld = " years old";
 
-const getAge = (birthDateObject) => {
-	const today = new Date();
-	const dob = new Date(birthDateObject);
-  
-	const yearsDifference = today.getFullYear() - dob.getFullYear();
-  
-	if (
-	  today.getMonth() < dob.getMonth() ||
-	  (today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate())
-	) {
-	  return yearsDifference - 1;
-	}
-  
-	return yearsDifference;
-  };
-  
- 
+var d = new Date();
+var n = d.getFullYear();
+function getAge(birthYear){
+	var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    age = currentYear - birthYear;
+    return age;
+}
+calculatedAge = getAge();
 
-let simplifier = function(person){
-	return myName + person.name + andIn +  person.Date + yearsOld;
-};
 
-people.forEach((person) =>{
-	return getAge(person)
-})
+function simplifier(person) {
+	return myName + person.name + andIn + getAge(person.birthDate.getFullYear()) + yearsOld;
+}
 
 console.log(people.map(simplifier));
--- exercises/20.6-Yes-and-no/app.js
+++ exercises/20.6-Yes-and-no/app.js
@@ -2,11 +2,14 @@ let theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1];
 
 //your code here
 let newArray = theBools.map((number) =>{
-    if(typeof number === 1){
-        newArray.push("wiki");
+    if(number === 1){
+        return "wiki";
     }
-    else if(typeof number === 0){
-        newArray.push("woko");
+    else if(number === 0){
+        return "woko";
     }
 });
+// newArray.forEach((item)=>{
+//     console.log(item);
+// });
 console.log(newArray);

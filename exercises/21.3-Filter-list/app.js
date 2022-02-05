0f6389652b727c86f2b0345ca307ef1c082916fe
willdodsonn <sodlax38@gmail.com>
2022-02-05 00:01:50 +0000

    arrays n maps commit

--- exercises/21.3-Filter-list/app.js
+++ exercises/21.3-Filter-list/app.js
@@ -1,11 +1,6 @@
 let names = ['Liam','Emma','Noah','Olivia','William','Ava','James','Isabella','Logan','Sophia','Benjamin','Mia','Mason','Charlotte','Elijah','Amelia','Oliver','Evelyn','Jacob','Abigail','Lucas','Harper','Michael','Emily','Alexander','Elizabeth','Ethan','Avery','Daniel','Sofia','Matthew','Ella','Aiden','Madison','Henry','Scarlett','Joseph','Victoria','Jackson','Aria','Samuel','Grace','Sebastian','Chloe','David','Camila','Carter','Penelope','Wyatt','Riley'];
 
-//declare your function here
-// const filterByName = (namesArray, searchCriteria) => {
-//     names.map((name) => {
-//         return name.toLowerCase().includes(searchCriteria);
-//     });
-// }
+
 
 
 const filterByName = (namesArray, searchCriteria) => {
--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,6 +1,11 @@
 // Code goes here
-
-
+function matrixBuilder(numOfRowsAndColumns){
+   for(let i = 0;  i < numOfRowsAndColumns.length; i++){
+       for(let j = 0; j < numOfRowsAndColumns[i].length; j++){
+        return(i, j);
+       }
+   }
+}
 
 // do not change anything from this line down
 console.log(matrixBuilder(5))
